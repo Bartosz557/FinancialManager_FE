bbc65553334e1a023955728593896be864b50a4c
Bartosz <bartosz.zurek.02@gmail.com>
2023-10-19 22:34:42 +0200

    dashboard test version added fixed routing

--- src/app/profilePage/profilePage.component.ts
+++ src/app/profilePage/profilePage.component.ts
@@ -1,9 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {ProfilePageService} from "./profilePage.service";
-
 
 @Component({
   selector: 'app-profile',
@@ -11,10 +10,19 @@ import {ProfilePageService} from "./profilePage.service";
   styleUrls: ['./profilePage.component.css']
 })
 
-export class ProfilePageComponent {
+export class ProfilePageComponent implements OnInit{
 
   constructor(private profilePageService: ProfilePageService,private router: Router) {
   }
+
+  ngOnInit() {
+    this.router.navigate(["/profile/test"])
+  }
+
+  navigateToProfile(){
+    this.router.navigate(["/profile/test"])
+  }
+
   logout() {
     this.profilePageService.logoutRequest().subscribe(
         (response: boolean) => {
@@ -25,8 +33,8 @@ export class ProfilePageComponent {
               this.unexpectedError()
         }
     )
-
   }
+
 
   succesLogout(){
     console.log("logout successfully")
