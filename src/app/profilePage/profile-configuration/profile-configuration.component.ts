62a0b939a21aafe422cc44391f8358cf9610110a
Bartosz <bartosz.zurek.02@gmail.com>
2023-11-01 15:24:27 +0100

    Configuration profile view - WIP

--- src/app/profilePage/profile-configuration/profile-configuration.component.ts
+++ src/app/profilePage/profile-configuration/profile-configuration.component.ts
@@ -6,5 +6,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile-configuration.component.css']
 })
 export class ProfileConfigurationComponent {
+  step: number = 1;
+
+  nextStep() {
+    if (this.step < 5) {
+      this.step++;
+    }
+  }
+  prevStep() {
+    if (this.step > 1) {
+      this.step--;
+    }
+  }
+  checkVisibility()
+  {
+    const welcomeText = document.getElementById('welcome-div')
+    if(this.step !== 1 && welcomeText != null){
+      welcomeText.style.display = 'none';
+    }else if(welcomeText !== null && welcomeText.style.display == 'none')
+      welcomeText.style.display = 'inline';
+    switch (this.step) {
+      case 1:
+        this.checkButton(false,false)
+        break;
+      case 2:
+        this.checkButton(false,true)
+        break;
+      case 3:
+        this.checkButton(true,true)
+        break;
+      case 4:
+        this.checkButton(true,true)
+        break;
+      case 5:
+        this.checkButton(true,false)
+        break;
+    }
+  }
+
+  checkButton(first: any, second: any) {
+    let visibility = first;
+    let button = document.getElementById('prev-button');
+    for (let i = 0; i < 2; i++) {
+      if (button != null) {
+        if (visibility) {
+          button.style.visibility = 'visible';
+          button.style.pointerEvents = 'auto';
+        } else {
+          button.style.visibility = 'hidden';
+          button.style.pointerEvents = 'none';
+        }
+      }
+      button = document.getElementById('next-button');
+      visibility = second;
+    }
+  }
 
 }
