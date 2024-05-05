09635bbd0ba7e4f4f0c3ccf99b631cee8205b6f1
Bartosz <bartosz.zurek.02@gmail.com>
2024-05-05 20:44:17 +0200

    ronaldinho gaucho bajksdhkasdjkasd

---
+++ src/app/profilePage/dashboard/dialogs/add-deposit-example-dialog/add-deposit-example-dialog.service.ts
@@ -0,0 +1,40 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
+import {Observable} from "rxjs";
+import {Injectable} from "@angular/core";
+
+@Injectable({providedIn: 'root'})
+export class AddDepositExampleDialogService{
+
+constructor(private http: HttpClient, private router: Router) {
+}
+
+  sendTransactionData(requestBody: any): Observable<boolean> {
+    return new Observable<boolean>((observer) => {
+      this.sendTransactionRequest(requestBody).subscribe(
+        (response) => {
+          if(response) {
+            observer.next(true);
+            observer.complete();
+          }else{
+            observer.error("Something went wrong");
+            observer.complete();
+          }
+        },
+        (error) => {
+          // this.unknownErrorMsg()
+          observer.error(error);
+          observer.complete();
+        }
+      );
+    });
+  }
+
+  private sendTransactionRequest(requestBody: any) {
+    return this.http.post('/api/v1/profile/dashboard/add-transaction',requestBody)
+
+  }
+
+}
---
+++ src/app/profilePage/dashboard/dialogs/add-deposit-example-dialog/add-deposit-example-dialog.ts
@@ -0,0 +1,52 @@
+import {Component, OnInit} from '@angular/core';
+import {MatDialogModule, MatDialogRef} from "@angular/material/dialog";
+import {DialogAnimationsExampleDialogService} from "../add-expense-dialog/dialog-animations-example-dialog.service";
+import {DialogAnimationsExampleDialog} from "../add-expense-dialog/dialog-animations-example-dialog";
+import {AddDepositExampleDialogService} from "./add-deposit-example-dialog.service";
+import {NgForOf} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+
+@Component({
+  selector: 'app-add-deposit-example-dialog',
+  templateUrl: './add-deposit-example-dialog.html',
+  styleUrls: ['./add-deposit-example-dialog.css'],
+  standalone: true,
+  imports: [
+    MatDialogModule,
+    NgForOf,
+    FormsModule
+  ]
+})
+export class AddDepositExampleDialog implements OnInit{
+  expenseValue: any;
+  expenseName: any;
+  constructor(public dialogRef: MatDialogRef<DialogAnimationsExampleDialog>, private dialogService: AddDepositExampleDialogService) {}
+  closeDialog() {
+    this.dialogRef.close({ success: false });
+  }
+
+  ngOnInit() {
+    this.dialogRef.updateSize('25%', '30%');
+  }
+  sendTransaction() {
+    const requestBody = {
+      expenseName: this.expenseName,
+      categoryName: "other",
+      transactionValue: this.expenseValue,
+      transactionType: "EXPENSE"
+    }
+    this.dialogService.sendTransactionData(requestBody).subscribe(
+      (success) => {
+        this.dialogRef.close({ success: true });
+        console.log(success)
+      },
+      (error) => {
+        console.log('Error:', error);
+      }
+    );
+
+
+
+    // TODO: Success window popup
+  }
+}
